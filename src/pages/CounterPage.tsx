3f54d4f51494b3ab3ca59a49dd5527ff9df6196b
JaacielBriseo <jaacielbris23@gmail.com>
2023-01-21 20:38:27 -0700

    Counter page with useReducer

--- src/pages/CounterPage.tsx
+++ src/pages/CounterPage.tsx
@@ -1,16 +1,73 @@
 import { Button } from '../components/Button';
-import { useCounter } from '../hooks/useCounter';
+import { useReducer } from 'react';
+import { Panel } from '../components/Panel';
 interface Props {
 	initialCount: number;
 }
+interface State {
+	count: number;
+	valueToAdd: number;
+}
+
+type Action =
+	| { type: 'increment' }
+	| { type: 'decrement' }
+	| { type: 'submit'; value: number }
+	| { type: 'change'; value: number };
+
+const counterReducer = (state: State, action: Action) => {
+	switch (action.type) {
+		case 'increment':
+			return { ...state, count: state.count + 1 };
+		case 'decrement':
+			return { ...state, count: state.count - 1 };
+		case 'submit':
+			return { ...state, count: state.count + action.value, valueToAdd: 0 };
+		case 'change':
+			return { ...state, valueToAdd: action.value };
+		default:
+			return state;
+	}
+};
 export const CounterPage = ({ initialCount }: Props) => {
-	const { count, handleClick } = useCounter(initialCount);
+	const [state, dispatch] = useReducer(counterReducer, { count: initialCount, valueToAdd: 0 });
+	const { count, valueToAdd } = state;
+
+	const increment = () => {
+		dispatch({ type: 'increment' });
+	};
+	const decrement = () => {
+		dispatch({ type: 'decrement' });
+	};
+	const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+		const value = Number(event.target.value);
+		dispatch({ type: 'change', value });
+	};
+	const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		dispatch({ type: 'submit', value: valueToAdd });
+	};
 	return (
-		<div>
-			<h1>Count is : {count}</h1>
-			<Button primary outline rounded onClick={handleClick}>
-				Increment
-			</Button>
-		</div>
+		<Panel className='m-3'>
+			<h1 className='text-lg'>Count is : {count}</h1>
+			<div className='flex'>
+				<Button primary outline rounded onClick={increment}>
+					Increment
+				</Button>
+				<Button secondary outline rounded onClick={decrement}>
+					Increment
+				</Button>
+			</div>
+			<form onSubmit={handleSubmit}>
+				<label>Add a lot!</label>
+				<input
+					value={valueToAdd || ''}
+					onChange={handleChange}
+					type='number'
+					className='p-1 m-3 bg-gray-50 border border-gray-300'
+				/>
+				<Button>Add it!</Button>
+			</form>
+		</Panel>
 	);
 };
